295484037cc79bba702030da21206028354953b7
Tyler Burkett <36940296+tyler-burkett@users.noreply.github.com>
2020-04-07 15:50:39 -0400

    Added example template and modified Jinja2 env to prevent syntax clash with Verilog

--- templates/vlib_nand2.v
+++ templates/vlib_nand2.v
@@ -1,19 +1,19 @@
-${header}
+{? header ?}
 
 `celldefine
-`timescale ${timescale}
+`timescale {? timescale ?}
 
-module ${lib}_nand2 (
-output ${out0},
+module {? lib ?}_nand2_{? drive ?} (
+output {? out0 ?},
 
-input ${in0},
-input ${in1}
+input {? in0 ?},
+input {? in1 ?}
 
 `ifdef SC_USE_PG_PIN
-, input ${vpwr0}
-, input ${vgnd0}
-, input ${vpb0}
-, input ${vnb0}
+, input {? vpwr0 ?}
+, input {? vgnd0 ?}
+, input {? vpb0 ?}
+, input {? vnb0 ?}
 `endif
 );
 
@@ -21,10 +21,10 @@ input ${in1}
 `else
 `ifdef SC_USE_PG_PIN
 `else
-supply1 ${vpwr0};
-supply0 ${vgnd0};
-supply1 ${vpb0};
-supply0 ${vnb0};
+supply1 {? vpwr0 ?};
+supply0 {? vgnd0 ?};
+supply1 {? vpb0 ?};
+supply0 {? vnb0 ?};
 `endif
 `endif
 
@@ -33,20 +33,20 @@ supply0 ${vnb0};
 reg csi_notifier;
 
 	specify
-		(${in0} +=> ${out0}) =  (0:0:0,0:0:0);
-		(${in0} -=> ${out0}) =  (0:0:0,0:0:0);
-		(${in1} +=> ${out0}) =  (0:0:0,0:0:0);
-		(${in1} -=> ${out0}) =  (0:0:0,0:0:0);
+		({? in0 ?} +=> {? out0 ?}) =  (0:0:0,0:0:0);
+		({? in0 ?} -=> {? out0 ?}) =  (0:0:0,0:0:0);
+		({? in1 ?} +=> {? out0 ?}) =  (0:0:0,0:0:0);
+		({? in1 ?} -=> {? out0 ?}) =  (0:0:0,0:0:0);
 	endspecify
 `endif
 
-  nand  ( UDP_IN_Y , ${in1} , ${in0} ) ;
+  nand  ( UDP_IN_Y , {? in1 ?} , {? in0 ?} ) ;
   `ifdef SC_USE_PG_PIN
 
-  ${lib}_pg_U_VPWR_VGND (UDP_OUT_Y, UDP_IN_Y, ${vpwr0}, ${vgnd0}) ;
-  buf  (${out0}, UDP_OUT_Y) ;
+  {? lib ?}_pg_U_VPWR_VGND (UDP_OUT_Y, UDP_IN_Y, {? vpwr0 ?}, {? vgnd0 ?}) ;
+  buf  ({? out0 ?}, UDP_OUT_Y) ;
   `else
-    buf ( ${out0} , UDP_IN_Y ) ;
+    buf ( {? out0 ?} , UDP_IN_Y ) ;
   `endif
 endmodule
 `endcelldefine
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import jinja2
 
-env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'),
+    variable_start_string="{?", variable_end_string="?}")
 template = env.get_template('vlib_nand2.v')
-print(template.render(lib='st8'))
+print(template.render(lib='st8', in0="A", in1= "B", out0="Z"))
