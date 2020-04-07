579f59af5534859e65afd124b8c46d6329518eff
Tyler Burkett <36940296+tyler-burkett@users.noreply.github.com>
2020-04-07 14:28:26 -0400

    Added Jinja2

---
+++ templates/vlib_nand2.v
@@ -0,0 +1,52 @@
+${header}
+
+`celldefine
+`timescale ${timescale}
+
+module ${lib}_nand2 (
+output ${out0},
+
+input ${in0},
+input ${in1}
+
+`ifdef SC_USE_PG_PIN
+, input ${vpwr0}
+, input ${vgnd0}
+, input ${vpb0}
+, input ${vnb0}
+`endif
+);
+
+`ifdef functional
+`else
+`ifdef SC_USE_PG_PIN
+`else
+supply1 ${vpwr0};
+supply0 ${vgnd0};
+supply1 ${vpb0};
+supply0 ${vnb0};
+`endif
+`endif
+
+`ifdef functional
+`else
+reg csi_notifier;
+
+	specify
+		(${in0} +=> ${out0}) =  (0:0:0,0:0:0);
+		(${in0} -=> ${out0}) =  (0:0:0,0:0:0);
+		(${in1} +=> ${out0}) =  (0:0:0,0:0:0);
+		(${in1} -=> ${out0}) =  (0:0:0,0:0:0);
+	endspecify
+`endif
+
+  nand  ( UDP_IN_Y , ${in1} , ${in0} ) ;
+  `ifdef SC_USE_PG_PIN
+
+  ${lib}_pg_U_VPWR_VGND (UDP_OUT_Y, UDP_IN_Y, ${vpwr0}, ${vgnd0}) ;
+  buf  (${out0}, UDP_OUT_Y) ;
+  `else
+    buf ( ${out0} , UDP_IN_Y ) ;
+  `endif
+endmodule
+`endcelldefine
--- tests/test.py
+++ tests/test.py
@@ -1,23 +1,5 @@
-import yaml
-import re
-import tempfile
+import jinja2
 
-with tempfile.TemporaryFile("a+") as final_yaml:
-    with open("tests/test_top.yml", "r") as top_file:
-        models = re.findall(r"\*([\w-]+)", top_file.read(), re.DOTALL)
-        if models is not None:
-            for match in set(models):
-                path = "tests/" + match + ".yml"
-                with open(path, "r") as model_file:
-                    final_yaml.write(model_file.read())
-        top_file.seek(0)
-        final_yaml.write(top_file.read())
-    final_yaml.seek(0)
-    print(final_yaml.read())
-    final_yaml.seek(0)
-    a = yaml.safe_load(final_yaml.read())["top"]
-    print(a)
-
-    final_yaml.seek(0)
-    with open("tests/test_top_out.yml", "w") as out_file:
-        yaml.safe_dump(a, out_file)
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+template = env.get_template('vlib_nand2.v')
+print(template.render(lib='st8'))
