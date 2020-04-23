a7375196dfd52822a7dd567b39d3412720f1ba22
Alex Schuster <56097774+apschuster@users.noreply.github.com>
2020-04-23 18:01:25 -0400

    ..

--- scs8hd/scs8hd_sedfxbp_2.v
+++ scs8hd/scs8hd_sedfxbp_2.v
@@ -1,4 +1,13 @@
-
+// Automatically edited by removePhraseFromMultipleFiles.pl 
+// iptguser	Tue Aug 23 13:13:19 EDT 2011 
+// \`suppress_faults;^\n 
+// \`ifdef TETRAMAX;^\n 
+// \`ifdef functional;\`else 
+// \`delay_mode_path;^\n 
+// \`disable_portfaults;\`nosuppress_faults 
+ 
+// Automatically modified by replacePGwithIfDef.pl 
+// iptguser	Tue Aug 23 12:32:33 EDT 2011
 
 
 module scs8hd_sedfxbp_2  (
