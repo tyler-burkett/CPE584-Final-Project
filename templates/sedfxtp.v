ba473580baaec4cb06b5213c5d2e8eb223a5b208
Tyler Burkett <36940296+tyler-burkett@users.noreply.github.com>
2020-05-01 22:37:21 -0400

    Finished diff test and fixed some issues in templates

--- templates/sedfxtp.v
+++ templates/sedfxtp.v
@@ -1,18 +1,21 @@
 {#
 	DESCRIPTION
-		
+
 	PARAMETER MAPPING
 		in[0]	:	CLK
 		in[1]	:	D
 		in[2]	:	DE
-		in[3]	:	SC
-		in[4]	:	SCD
-		in[5]	:	SCE
+		in[3]	:	SCD
+		in[4]	:	SCE
 		out[0]	:	Q
 -#}
 
 
 {? header ?}
+
+`celldefine
+`timescale 1ns / 1ps
+
 
 
 module {? lib ?}_{? name ?}_{? drive ?}  (
@@ -20,8 +23,8 @@ output {? out[0] ?},
 input {? in[0] ?},
 input {? in[1] ?},
 input {? in[2] ?},
-input {? in[4] ?},
-input {? in[5] ?}
+input {? in[3] ?},
+input {? in[4] ?}
 
 
 `ifdef SC_USE_PG_PIN
@@ -48,46 +51,46 @@ supply0 {? vnb[0] ?};
   wire buf_Q;
 
 `ifdef functional
-  {? lib ?}_pg_U_MUX_2_1 ( mux_out , {? in[2] ?}${? in[1] ?} , {? in[4] ?} , {? in[5] ?} ) , ( {? in[2] ?}${? in[1] ?} , buf_Q , {? in[1] ?} , {? in[2] ?} ) ; 
+  {? lib ?}_pg_U_MUX_2_1 ( mux_out , {? in[2] ?}${? in[1] ?} , {? in[3] ?} , {? in[4] ?} ) , ( {? in[2] ?}${? in[1] ?} , buf_Q , {? in[1] ?} , {? in[2] ?} ) ;
   `ifdef SC_USE_PG_PIN
    {? lib ?}_pg_U_DF_P_NO_pg #0.001 ( buf_Q , mux_out , {? in[0] ?} ,  , {? vpwr[0] ?} , {? vgnd[0] ?} ) ;
   `else
    {? lib ?}_pg_U_DF_P #0.001 ( buf_Q , mux_out , {? in[0] ?} ) ;
-  `endif 
+  `endif
 `else
-  reg notifier ; 
+  reg notifier ;
   wire D_delayed;
   wire DE_delayed;
   wire SCD_delayed;
   wire SCE_delayed;
   wire CLK_delayed;
-  {? lib ?}_pg_U_MUX_2_1 ( mux_out , {? in[2] ?}${? in[1] ?} , SCD_delayed , SCE_delayed ) , ( {? in[2] ?}${? in[1] ?} , buf_Q , D_delayed , DE_delayed ) ; 
-  {? lib ?}_pg_U_DF_P_NO_pg ( buf_Q , mux_out , CLK_delayed , notifier , {? vpwr[0] ?} , {? vgnd[0] ?} ) ; 
+  {? lib ?}_pg_U_MUX_2_1 ( mux_out , {? in[2] ?}${? in[1] ?} , SCD_delayed , SCE_delayed ) , ( {? in[2] ?}${? in[1] ?} , buf_Q , D_delayed , DE_delayed ) ;
+  {? lib ?}_pg_U_DF_P_NO_pg ( buf_Q , mux_out , CLK_delayed , notifier , {? vpwr[0] ?} , {? vgnd[0] ?} ) ;
 
   wire AWAKE;
   wire COND1;
   wire COND2;
   wire COND3;
-  assign AWAKE = ( {? vpwr[0] ?} === 1'b1 ) ; 
-  assign COND1 = ( AWAKE && ( SCE_delayed === 1'b0 ) && ( DE_delayed === 1'b1 ) ) ; 
-  assign COND2 = ( AWAKE && ( SCE_delayed === 1'b1 ) ) ; 
-  assign COND3 = ( AWAKE && ( DE_delayed === 1'b1 ) && ( D_delayed !== SCD_delayed ) ) ; 
+  assign AWAKE = ( {? vpwr[0] ?} === 1'b1 ) ;
+  assign COND1 = ( AWAKE && ( SCE_delayed === 1'b0 ) && ( DE_delayed === 1'b1 ) ) ;
+  assign COND2 = ( AWAKE && ( SCE_delayed === 1'b1 ) ) ;
+  assign COND3 = ( AWAKE && ( DE_delayed === 1'b1 ) && ( D_delayed !== SCD_delayed ) ) ;
   specify
     ( posedge {? in[0] ?} => ( {? out[0] ?} : {? in[0] ?} ) ) = ( 0:0:0 , 0:0:0 ) ; // delays are tris , tfall
-    $setuphold ( posedge {? in[0] ?} , posedge {? in[2] ?} , 0:0:0 , 0:0:0 , notifier , AWAKE , AWAKE , CLK_delayed , DE_delayed ) ; 
-    $setuphold ( posedge {? in[0] ?} , negedge {? in[2] ?} , 0:0:0 , 0:0:0 , notifier , AWAKE , AWAKE , CLK_delayed , DE_delayed ) ; 
-    $setuphold ( posedge {? in[0] ?} , posedge {? in[1] ?} , 0:0:0 , 0:0:0 , notifier , COND1 , COND1 , CLK_delayed , D_delayed ) ; 
-    $setuphold ( posedge {? in[0] ?} , negedge {? in[1] ?} , 0:0:0 , 0:0:0 , notifier , COND1 , COND1 , CLK_delayed , D_delayed ) ; 
-    $setuphold ( posedge {? in[0] ?} , posedge {? in[4] ?} , 0:0:0 , 0:0:0 , notifier , COND2 , COND2 , CLK_delayed , SCD_delayed ) ; 
-    $setuphold ( posedge {? in[0] ?} , negedge {? in[4] ?} , 0:0:0 , 0:0:0 , notifier , COND2 , COND2 , CLK_delayed , SCD_delayed ) ; 
-    $setuphold ( posedge {? in[0] ?} , posedge {? in[5] ?} , 0:0:0 , 0:0:0 , notifier , COND3 , COND3 , CLK_delayed , SCE_delayed ) ; 
-    $setuphold ( posedge {? in[0] ?} , negedge {? in[5] ?} , 0:0:0 , 0:0:0 , notifier , COND3 , COND3 , CLK_delayed , SCE_delayed ) ; 
-    $width ( posedge {? in[0] ?} &&& AWAKE , 1.0:1.0:1.0 , 0 , notifier ) ; 
-    $width ( negedge {? in[0] ?} &&& AWAKE , 1.0:1.0:1.0 , 0 , notifier ) ; 
+    $setuphold ( posedge {? in[0] ?} , posedge {? in[2] ?} , 0:0:0 , 0:0:0 , notifier , AWAKE , AWAKE , CLK_delayed , DE_delayed ) ;
+    $setuphold ( posedge {? in[0] ?} , negedge {? in[2] ?} , 0:0:0 , 0:0:0 , notifier , AWAKE , AWAKE , CLK_delayed , DE_delayed ) ;
+    $setuphold ( posedge {? in[0] ?} , posedge {? in[1] ?} , 0:0:0 , 0:0:0 , notifier , COND1 , COND1 , CLK_delayed , D_delayed ) ;
+    $setuphold ( posedge {? in[0] ?} , negedge {? in[1] ?} , 0:0:0 , 0:0:0 , notifier , COND1 , COND1 , CLK_delayed , D_delayed ) ;
+    $setuphold ( posedge {? in[0] ?} , posedge {? in[3] ?} , 0:0:0 , 0:0:0 , notifier , COND2 , COND2 , CLK_delayed , SCD_delayed ) ;
+    $setuphold ( posedge {? in[0] ?} , negedge {? in[3] ?} , 0:0:0 , 0:0:0 , notifier , COND2 , COND2 , CLK_delayed , SCD_delayed ) ;
+    $setuphold ( posedge {? in[0] ?} , posedge {? in[4] ?} , 0:0:0 , 0:0:0 , notifier , COND3 , COND3 , CLK_delayed , SCE_delayed ) ;
+    $setuphold ( posedge {? in[0] ?} , negedge {? in[4] ?} , 0:0:0 , 0:0:0 , notifier , COND3 , COND3 , CLK_delayed , SCE_delayed ) ;
+    $width ( posedge {? in[0] ?} &&& AWAKE , 1.0:1.0:1.0 , 0 , notifier ) ;
+    $width ( negedge {? in[0] ?} &&& AWAKE , 1.0:1.0:1.0 , 0 , notifier ) ;
   endspecify
 `endif
 
-buf ( {? out[0] ?} , buf_Q ) ; 
+buf ( {? out[0] ?} , buf_Q ) ;
 
 endmodule
 `endcelldefine
--- tests/generate_verilog/generate_verilog_test.py
+++ tests/generate_verilog/generate_verilog_test.py
@@ -5,15 +5,21 @@ import re
 import subprocess
 import sys
 import shutil
+import difflib
 
 class GenerateVerilogTest(unittest.TestCase):
     @classmethod
-    def setUpClass(self):
+    def setUpClass(cls):
         # Run generate verilog script on test.yml and deposit
         # verilog results in this test directory
         dir_path = os.path.dirname(os.path.normpath(__file__))
         test_yml = "./tests/data/scs8hd.yml"
         subprocess.call([sys.executable, "./generate_verilog.py", "-m", "-d{}".format(dir_path), test_yml])
+
+        # Setup paths
+        cls.dir_path = os.path.dirname(os.path.normpath(__file__))
+        cls.test_models_path = "tests/data/scs8hd"
+        cls.gen_models_path = os.path.normpath(os.path.join(dir_path + os.sep, os.path.basename(cls.test_models_path)))
 
     def test_script_outputs(self):
         # Make test do nothing until we can test on proper verilog sim setup
@@ -35,23 +41,27 @@ class GenerateVerilogTest(unittest.TestCase):
                 self.fail()
 
     def test_script_diff(self):
-        # Check the models in the test models and confirm that they
-        # match the generated ones
-        dir_path = os.path.dirname(os.path.normpath(__file__))
-        test_models_path = "tests/data/scs8hd"
-        self.gen_models_path = os.path.normpath(os.path.join(dir_path + os.sep, os.path.basename(test_models_path)))
-        files_to_check = os.listdir(test_models_path)
+        # Check the models in the generated models and confirm that they
+        # match the test ones
+        files_to_check = os.listdir(self.gen_models_path)
         for model in files_to_check:
-            test_model = os.path.abspath(os.path.join(test_models_path + os.sep, model))
+            test_model = os.path.abspath(os.path.join(self.test_models_path + os.sep, model))
             gen_model = os.path.abspath(os.path.join(self.gen_models_path + os.sep, model))
             spaces_and_comments = r"\s+|(?m)^ *//.*\n?"
             with open(test_model, "r") as test_file:
                 test_model_text = re.sub(spaces_and_comments, "", test_file.read())
             with open(gen_model, "r") as gen_file:
                 gen_model_text = re.sub(spaces_and_comments, "", gen_file.read())
-            if test_model_text != gen_model_text:
+            diffchecker = difflib.SequenceMatcher(None, test_model_text, gen_model_text)
+            if diffchecker.ratio() < 0.98:
                 self.fail("Error in generated model {}".format(gen_model))
+            if diffchecker.ratio() != 1:
+                print("Differences found between {0} and {1}".format(test_model, gen_model))
+                for tag, i1, i2, j1, j2 in diffchecker.get_opcodes():
+                    if (test_model_text[i1:i2] != "" or gen_model_text[j1:j2] != "") and tag != "equal":
+                        print("{0} test_model[{1}:{2}] ({3}) gen_model[{4}:{5}] ({6})".format(tag, i1, i2, test_model_text[i1:i2], j1, j2, gen_model_text[j1:j2]))
 
-    def tearDownClass(self):
-        if os.path.exists(self.gen_models_path):
-            shutil.rmtree(self.gen_models_path)
+    @classmethod
+    def tearDownClass(cls):
+        if os.path.exists(cls.gen_models_path):
+            shutil.rmtree(cls.gen_models_path)
